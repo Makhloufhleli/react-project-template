3522fb51d3b79676930b390ee8b60b8a1f6e0b33
Makhloufhleli <makhlouf.hleli@gmail.com>
2022-07-20 17:34:52 +0200

    create theme colors config

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -1,1 +1,21 @@
-export default {};
+import { alpha } from "@mui/material/styles";
+
+function createGradient(color1: string, color2: string) {
+  return `linear-gradient(to bottom, ${color1}, ${color2})`;
+}
+
+const PRIMARY = {
+  main: "#E41B12",
+  light: "#EE0000",
+  lighter: "#FF0000",
+  dark: "#DD2211",
+  darker: "#AA3333",
+};
+
+const SECONDARY = {
+  lighter: "#D6E4FF",
+  light: "#84A9FF",
+  main: "#3366FF",
+  dark: "#1939B7",
+  darker: "#091A7A",
+};
